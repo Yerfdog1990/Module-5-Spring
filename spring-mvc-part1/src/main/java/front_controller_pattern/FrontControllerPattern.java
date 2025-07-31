bd48798d8d37a3de2e1a9e38fcb719686ad39fe1
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-07-31 12:29:28 +0300

    Spring MVC part 1

--- spring-mvc-part1/src/main/java/front_controller_pattern/FrontControllerPattern.java
+++
@@ -1,9 +0,0 @@
-package front_controller_pattern;
-
-public class FrontControllerPattern {
-    public static void main(String[] args) {
-        FrontController frontController = new FrontController();
-        frontController.dispatchRequest("benz");
-        frontController.dispatchRequest("volkswagen");
-    }
-}
