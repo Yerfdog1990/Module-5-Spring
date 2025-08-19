f7f41ffa9471dbc06dba14dab87814d236bb6b86
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-08-20 05:29:39 +0700

    Spring bean configuration using xml file

--- spring-framework/src/main/java/springxmlconfiguration/Customer.java
+++ spring-framework/src/main/java/springxmlconfiguration/Customer.java
@@ -1,4 +1,13 @@
 package springxmlconfiguration;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Customer {
+  public static void main(String[] args) {
+    ClassPathXmlApplicationContext applicationContext =
+        new ClassPathXmlApplicationContext("bean.xml");
+    Employee employee = (Employee) applicationContext.getBean("employee");
+    employee.display();
+    System.out.println(employee);
+  }
 }
--- spring-framework/src/main/java/springxmlconfiguration/Employee.java
+++ spring-framework/src/main/java/springxmlconfiguration/Employee.java
@@ -1,4 +1,13 @@
 package springxmlconfiguration;
 
+import lombok.Data;
+
+@Data
 public class Employee {
+  private String firstName = "Godfrey";
+  private String lastName = "Ouma";
+
+  public void display() {
+    System.out.println("My name is " + firstName + " " + lastName);
+  }
 }
