be8766d8448c5dc8a5ea9b9fa6a16fb299d2692b
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-08-20 20:04:39 +0700

    Chore: Created HelloController class and tested it using tomcat.

---
+++ spring-webapp-demo/src/main/java/springweb/controller/HelloController.java
@@ -0,0 +1,14 @@
+package springweb.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  @GetMapping(path = "/hello", produces = MediaType.TEXT_PLAIN_VALUE)
+  public String hello() {
+    return "Hello World from spring web";
+  }
+}
