45e52a8be97688fb4449fa1e89fdda0d70472e54
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-08-20 20:04:39 +0700

    Chore: Created HelloController class and tested it using tomcat.

---
+++ spring-webapp-demo/src/main/java/springweb/config/AppConfig.java
@@ -0,0 +1,8 @@
+package springweb.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class AppConfig {}
