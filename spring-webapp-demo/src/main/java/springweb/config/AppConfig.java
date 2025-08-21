8f511f7970527b9227b36da81e0671578a073863
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-08-21 18:01:11 +0700

    feat(Thymleaf): Add Thymleaf integration to the project.

--- spring-webapp-demo/src/main/java/springweb/config/AppConfig.java
+++ spring-webapp-demo/src/main/java/springweb/config/AppConfig.java
@@ -1,8 +1,42 @@
 package springweb.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.thymeleaf.spring5.ISpringTemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @Configuration
 @EnableWebMvc
-public class AppConfig {}
+public class AppConfig {
+  @Bean
+  public ITemplateResolver templateResolver() {
+    SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+    resolver.setPrefix("classpath:templates/");
+    resolver.setSuffix(".html");
+    resolver.setTemplateMode(TemplateMode.HTML);
+    resolver.setCharacterEncoding("UTF-8");
+    return resolver;
+  }
+
+  @Bean
+  public ISpringTemplateEngine templateEngine(ITemplateResolver templateResolver) {
+    SpringTemplateEngine engine = new SpringTemplateEngine();
+    engine.setTemplateResolver(templateResolver);
+    engine.setEnableSpringELCompiler(true);
+    return engine;
+  }
+
+  @Bean
+  public ViewResolver viewResolver(ISpringTemplateEngine templateEngine) {
+    ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+    resolver.setTemplateEngine(templateEngine);
+    resolver.setCharacterEncoding("UTF-8");
+    return resolver;
+  }
+}
