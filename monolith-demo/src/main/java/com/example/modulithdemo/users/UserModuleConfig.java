1f57f5aefbc3a7699bc7feedf1064f1fe6dc82c3
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-09-06 10:22:21 +0700

    Spring modulith demo

---
+++ monolith-demo/src/main/java/com/example/modulithdemo/users/UserModuleConfig.java
@@ -0,0 +1,9 @@
+package com.example.modulithdemo.users;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.modulith.ApplicationModule;
+
+@Configuration
+@ApplicationModule(displayName = "Users module")
+public class UserModuleConfig {
+}
---
+++ monolith-demo/src/test/java/com/example/modulithdemo/MonolithDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modulithdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
